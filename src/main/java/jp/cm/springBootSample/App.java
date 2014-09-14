059c68ddc68e29588aa93dce50fb49e8c2afc904
tempest200903 <tempest200903@gmail.com>
2014-09-14 22:41:00 +0900

    mvn archetype:create -DgroupId=jp.cm.springBootSample -DartifactId=20140914-springboot-experimental

---
+++ src/main/java/jp/cm/springBootSample/App.java
@@ -0,0 +1,13 @@
+package jp.cm.springBootSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
